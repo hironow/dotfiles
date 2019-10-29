2fa96329778a4339cd6c4190732e52521b1bcaaf
hironow <hironow365@gmail.com>
2019-10-30 03:07:19 +0900

    lint

--- install.sh
+++ install.sh
@@ -4,46 +4,46 @@ set -eu
 DOTPATH=$HOME/dotfiles
 
 if [ ! -d "$DOTPATH" ]; then
-    git clone https://github.com/hironow/dotfiles.git "$DOTPATH"
+  git clone https://github.com/hironow/dotfiles.git "$DOTPATH"
 else
-    echo "$DOTPATH already downloaded. Updating..."
-    cd "$DOTPATH"
-    git stash
-    git checkout master
-    git pull origin master
+  echo "$DOTPATH already downloaded. Updating..."
+  cd "$DOTPATH"
+  git stash
+  git checkout master
+  git pull origin master
 fi
 
 cd "$DOTPATH"
 
 # install homebrew
-if ! which brew > /dev/null; then 
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! command -v brew >/dev/null; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 brew bundle
 
 # install google cloud sdk
-if ! which gcloud > /dev/null; then
-    curl https://sdk.cloud.google.com | bash -s -- --disable-prompts --install-dir="$HOME"
-    gcloud components install \
-		app-engine-go \
-		beta \
-		cloud-datastore-emulator \
-		pubsub-emulator
+if ! command -v gcloud >/dev/null; then
+  curl https://sdk.cloud.google.com | bash -s -- --disable-prompts --install-dir="$HOME"
+  gcloud components install \
+    app-engine-go \
+    beta \
+    cloud-datastore-emulator \
+    pubsub-emulator
 fi
 
 # install yarn global packages
-if ! which yarn > /dev/null; then
-    yarn global add \
-        gatsby-cli \
-        json-server \
-        prettier \
-        surge \
-        typescript \
-        fx \
-        serverless \
-        http-server \
-        mdx-deck \
-        tabtab
+if ! command -v yarn >/dev/null; then
+  yarn global add \
+    gatsby-cli \
+    json-server \
+    prettier \
+    surge \
+    typescript \
+    fx \
+    serverless \
+    http-server \
+    mdx-deck \
+    tabtab
 fi
 
 make clean
