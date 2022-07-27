824266781bc3e72175a99631cf2df412add87434
hironow <hironow365@gmail.com>
2022-07-27 15:05:08 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,10 @@ cd "$DOTPATH"
 # install homebrew
 if ! command -v brew >/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
+
 make add-brew
 
 # install google cloud sdk
