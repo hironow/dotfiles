9a03ee6c299535c654b06aa92f5706d7c948e3d6
hironow <hironow365@gmail.com>
2022-07-27 15:14:00 +0900

    fix ci

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ cd "$DOTPATH"
 # install homebrew
 if ! command -v brew >/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME"/.zprofile
+  echo "eval "$(/opt/homebrew/bin/brew shellenv)"" >> "$HOME"/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
