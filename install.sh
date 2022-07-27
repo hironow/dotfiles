249d2dd48abb00d75546f5e983f710942a0e4566
hironow <hironow365@gmail.com>
2022-07-27 15:59:00 +0900

    rm zprofile

--- install.sh
+++ install.sh
@@ -19,7 +19,6 @@ cd "$DOTPATH"
 # install homebrew
 if ! command -v brew >/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo "eval $(/opt/homebrew/bin/brew shellenv)" >> "$HOME"/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
